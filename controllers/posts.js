1cf82323ed7e2955437c1b94eeeca7831704c852
SergioV <sergioscode@gmail.com>
2022-03-30 05:11:15 -0700

    Completed connection from the client side to the database.

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,7 @@ import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
   try {
-    const postMessages = await PostMessage.find({});
+    const postMessages = await PostMessage.find();
 
     console.log(postMessages);
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postsRoutes from "./routes/posts.js";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
@@ -11,10 +11,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postsRoutes);
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://sergioscode:123456Sv$@cluster0.sgkpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://sergioscode:123456Sv@cluster0.sgkpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
