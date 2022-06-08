82f6fb1cecba80db20f9c120be70b567164ee56e
SergioV <sergioscode@gmail.com>
2022-06-08 05:35:45 -0700

    make it better

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ import userRoutes from "./routes/users.js";
 const app = express();
 // dotenv.config();
 
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
+
+app.get("/", (res, req) => {
+  res.send("APP IS RUNNING");
+});
 
 const CONNECTION_URL =
   "mongodb+srv://sergioscode:123456Sv@cluster0.sgkpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
