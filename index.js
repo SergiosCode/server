4c4e65b0ac3ac7ad2ff52abf079fbd582552a174
SergioV <sergioscode@gmail.com>
2022-06-10 05:24:02 -0700

    //

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-app.get("/", function(res) {
+app.get("/", function(res, req) {
   res.send("Hello World");
 });
 
@@ -24,6 +24,8 @@ const CONNECTION_URL =
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
+
+mongoose.set("");
