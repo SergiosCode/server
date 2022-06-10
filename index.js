25415c775b533b02552c079daf21e4157100e7ea
SergioV <sergioscode@gmail.com>
2022-06-10 04:07:09 -0700

    ./

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-// import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
-// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -30,6 +28,5 @@ mongoose
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
 
-// module.exports = app;
